df59c4d20fd9febb85f68a7361f92324b2895289
mattgrayy <Matt2.Gray@live.uwe.ac.uk>
2016-10-31 14:08:41 +0000

    extra resources

--- TerrainDeformationProject/NewDirectXTK old/Game/App.cpp
+++ TerrainDeformationProject/NewDirectXTK old/Game/App.cpp
@@ -45,7 +45,7 @@ HRESULT App::InitWindow( HINSTANCE _hInstance, int _nCmdShow )
     wcex.hCursor = LoadCursor( nullptr, IDC_ARROW );
     wcex.hbrBackground = ( HBRUSH )( COLOR_WINDOW + 1 );
     wcex.lpszMenuName = nullptr;
-    wcex.lpszClassName = L"GEAWindowClass";
+    wcex.lpszClassName = L"GPU_Terrain_Deformation";
     wcex.hIconSm = LoadIcon( wcex.hInstance, ( LPCTSTR )IDI_TUTORIAL1 );
     if( !RegisterClassEx( &wcex ) )
         return E_FAIL;
@@ -55,7 +55,7 @@ HRESULT App::InitWindow( HINSTANCE _hInstance, int _nCmdShow )
 #ifdef DEBUG
     RECT rc = { 0, 0, 800, 600 };
     AdjustWindowRect( &rc, WS_OVERLAPPEDWINDOW, FALSE );
-    m_hWnd = CreateWindow( L"GEAWindowClass", L"GEA GROUP PROJECT", WS_OVERLAPPEDWINDOW,
+    m_hWnd = CreateWindow( L"GPU_Terrain_Deformation", L"GPU_Terrain_Deformation", WS_OVERLAPPEDWINDOW,
                            600, 100, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, _hInstance,
                            nullptr );
 #else
@@ -63,8 +63,8 @@ HRESULT App::InitWindow( HINSTANCE _hInstance, int _nCmdShow )
 	int SCREEN_HEIGHT = 900;
 	//Go to Fullscreen in Release
 	m_hWnd = CreateWindowEx(NULL,
-		L"GEAWindowClass",
-		L"GEA GROUP PROJECT",
+		L"GPU_Terrain_Deformation",
+		L"GPU_Terrain_Deformation",
 		WS_EX_TOPMOST | WS_POPUP,    // fullscreen values
 		0, 0,    // the starting x and y positions should be 0
 		SCREEN_WIDTH, SCREEN_HEIGHT,
